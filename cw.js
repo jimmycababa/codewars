9c697569e6f3984cc141bc7f66968a499adc2d02
jimmycababa <jimmycababa@gmail.com>
2021-04-04 22:23:06 -0500

    codewars 4/4

--- cw.js
+++ cw.js
@@ -100,3 +100,21 @@ var replaceDots = function(str){
     return str.split('.').join('-')
 }
 
+// the company you work for has just been awarded a contract to build a payment gateway. In order to help move things along, you have volunteered to create a function that will take a float and return the amount formatting in dollars and cents. 39.99 becomes $39.99
+// the rest of your team will make sure that the argument is sanitized before being passed to your function although you will need to account for adding trailing zeros if they are missing (though you wont have to worry about a dangling period)
+
+function formatMoney(amount){
+    return "$" + Number(amount).toFixed(2)
+}
+
+// Nathan loves cycling.
+
+// Because Nathan knows it is important to stay hydrated, he drinks 0.5 litres of water per hour of cycling.
+
+// You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
+function litres(time) {
+    let litres = .5;
+    return Math.floor(litres * time)
+  }
+
+
