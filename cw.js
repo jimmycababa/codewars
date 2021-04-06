14186a67aeda710e8aed4fb035e2079eb8bab699
jimmycababa <jimmycababa@gmail.com>
2021-04-06 11:02:51 -0500

    codewars 4/6

--- cw.js
+++ cw.js
@@ -165,4 +165,8 @@ function howMuchILoveYou(nbPetals){
     }
 }
 
-
+// There's a "3 for 2" (or "2+1" if you like) offer on mangoes. For a given quantity and price (per mango), calculate the total cost of the mangoes.
+function mango(quantity, price){
+    let answer = quantity - Math.floor(quantity / 3)
+    return answer * price
+    }
