9abea0df546fb4d004fc900df97405776388dad7
jimmycababa <jimmycababa@gmail.com>
2021-04-11 15:01:22 -0500

    codewars 4/11

--- cw.js
+++ cw.js
@@ -259,3 +259,18 @@ function repeater(string, n){
 
 def quotable(name, quote):
 return name + ' said ' + '"'+quote+'"'
+
+// Beautiful is better than ugly.
+// Explicit is better than implicit.
+// Simple is better than complex.
+// Complex is better than complicated.
+
+// Write a function, format_poem() that takes a string like the one line example as an argument and returns a new string that is formatted across multiple lines with each new sentence starting on a new line when you print it out.
+
+// Try to solve this challenge with the str.split() and the str.join() string methods.
+
+// Every sentence will end with a period, and every new sentence will have one space before the previous period. Be careful about trailing whitespace in your solution.
+
+function formatPoem(poem) {
+    return poem.split('. ').join('.\n')
+  }
