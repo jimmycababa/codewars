b8d8a0957daa38dc983bfdeb042bdd6204357693
jimmycababa <jimmycababa@gmail.com>
2021-04-14 15:48:35 -0500

    codewars 4/14

--- cw.js
+++ cw.js
@@ -361,3 +361,11 @@ class SmallestIntegerFinder{
         return Math.min.apply(Math, args);
     }
 }
+
+// Write a function to get the first elements of asequence. Passing a parameter n (default=1) will return the first n elements of the sequence.
+
+// If n == 0 return an empty sequence []
+
+function first(arr, n) {
+    return arr.slice(0,n || n !== 0) 
+  }
