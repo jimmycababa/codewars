42279f085b77891fa57fa941148e03bee5aa842f
jimmycababa <jimmycababa@gmail.com>
2021-04-17 20:41:52 -0500

    codewars 4/17

--- cw.js
+++ cw.js
@@ -418,3 +418,15 @@ function bmi(weight, height) {
         return "Obese"
       }
   }
+
+//   Write function splitSentence which will create a list of strings from a string.
+
+// Example:
+
+// "hello world" -> ["hello", "world"]
+
+// solution
+function splitSentence(s) {
+    let answer =  s.split(' ');
+    return answer
+  }
