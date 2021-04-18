119fd39dd2a51a08d0d16136a3b1f2d35000d615
jimmycababa <jimmycababa@gmail.com>
2021-04-17 20:40:13 -0500

    codewars 4/17

--- cw.js
+++ cw.js
@@ -395,3 +395,26 @@ function spam(number){
     let total = 'hue';
     return total.repeat(number)
 }
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+// solution
+function bmi(weight, height) {
+    let answer = weight / (height**2)
+      if(answer <= 18.5){
+        return "Underweight"
+      }else if(answer <= 25.0){
+        return "Normal"
+      }else if(answer <= 30.0){
+        return "Overweight"
+      }else{
+        return "Obese"
+      }
+  }
