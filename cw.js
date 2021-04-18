33991d4cdd15846f45c76edb6a799fc1e79ff885
jimmycababa <jimmycababa@gmail.com>
2021-04-17 20:43:36 -0500

    codewars 4/17

--- cw.js
+++ cw.js
@@ -430,3 +430,14 @@ function splitSentence(s) {
     let answer =  s.split(' ');
     return answer
   }
+
+//   JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+// The solution would work like the following:
+
+// getEvenNumbers([2,4,5,6]) // should == [2,4,6]
+
+// solution
+function getEvenNumbers(numbersArray){
+    return numbersArray.filter(num => num % 2 === 0)
+  }
