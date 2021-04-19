2f1d107444888c03cf8a27c829ce4800457aab0a
jimmycababa <jimmycababa@gmail.com>
2021-04-19 14:29:14 -0500

    codewars 4/19

--- cw.js
+++ cw.js
@@ -465,3 +465,15 @@ function disemvowel(str) {
 function findOdd(A) {
     return A.reduce((a,b) => a ^ b))
 }
+
+// 4/19/2021
+// You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+// You can assume all values in the array are numbers.
+
+// solution
+
+function smallEnough(a, limit){
+  let answer = [];
+    return a.every(answer => answer <= limit) ? true : false
+}
