b9709faa55ea25ef136daae3c8cfb6c8e222e3fa
jimmycababa <jimmycababa@gmail.com>
2021-04-19 15:41:20 -0500

    codewars 4/19

--- cw.js
+++ cw.js
@@ -477,3 +477,37 @@ function smallEnough(a, limit){
   let answer = [];
     return a.every(answer => answer <= limit) ? true : false
 }
+
+// Complete function howManydays, function accept 1 parameters:month, means the month of year, different month has different days (refer to the following table), return a number that how many days in this month(month is always greater than 0, less than or equal to 12).
+
+// solution
+function howManydays(month){
+  switch (month){
+  case 1:
+      return Number('31')
+  case 2:
+      return Number('28')
+  case 3:
+      return Number('31')
+  case 4:
+      return Number('30')
+  case 5:
+      return Number('31')
+  case 6:
+      return Number ('30')
+  case 7:
+      return Number('31')
+  case 8:
+      return Number('31')
+  case 9:
+      return Number('30')
+  case 10:
+      return Number('31')
+  case 11:
+      return Number('30')
+  case 12:
+      return Number('31')
+      break;
+  }
+  
+}
