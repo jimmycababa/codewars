43c6b826d5f2393259734c28500ec07d186b4975
jimmycababa <jimmycababa@gmail.com>
2021-04-20 09:23:40 -0500

    codewars 4/20

--- cw.js
+++ cw.js
@@ -529,3 +529,21 @@ const quartaerOf = (month => {
     return 4
   }
 })
+
+// 4/20/2021
+
+// Coding in function bigToSmall. function accept 1 parameter arr(2D number array).
+
+// Your task is: First to all, refer to the example above, flat output arr to a one-dimensional array.
+
+// And then sort array in descending order.
+
+// Finally, use the separator ">" to connect the elements into a string.
+
+// Don't complain about the situation like 1>1 is not reasonable, it is just a separator.
+
+// solution
+function bigToSmall(arr){
+  let result = [].concat(...arr)
+  return result.sort((a,b) => b - a).join('>')
+}
