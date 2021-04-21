ba1367aa1bff2ca40d6d157f9c95390beffcef0d
jimmycababa <jimmycababa@gmail.com>
2021-04-21 14:31:13 -0500

    codewars 4/21

--- cw.js
+++ cw.js
@@ -586,3 +586,12 @@ function check(a,x){
   return a.includes(x) ? true : false
 };
 
+// You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
+
+// solution
+function reverse(string){
+  let answer = string.split(' ').reverse(',').join(' ');
+  return answer
+}
+
+
