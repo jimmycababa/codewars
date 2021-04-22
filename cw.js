3637c564dc3f98e19f2ab1e1a80e4a9d37566647
jimmycababa <jimmycababa@gmail.com>
2021-04-22 09:00:10 -0500

    codewars 4/22

--- cw.js
+++ cw.js
@@ -594,4 +594,18 @@ function reverse(string){
   return answer
 }
 
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// Patrick Feeney => P.F
+
+// solution
+function abbrevName(name){
+  return name.split(' ').map(i => i.charAt(0)).join('.').toUpperCase()
+  }
 
