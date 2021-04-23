813ec2a68af0e4701c0dcf66a31e7580cdf52f8f
jimmycababa <jimmycababa@gmail.com>
2021-04-23 11:28:44 -0500

    codewars 4/23

--- cw.js
+++ cw.js
@@ -633,3 +633,16 @@ function DNAtoRNA(dna) {
   let re = /T/gi;
   return dna.replace(re, 'U')
   }
+
+  // Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+// Names given are always valid strings.
+// solution
+function areYouPlayingBanjo(name) {
+  return name.startsWith('r'.toUpperCase()) || name.startsWith('R'.toLowerCase()) ? name + " plays banjo" : name + " does not play banjo"
+}
