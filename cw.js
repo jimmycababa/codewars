ecef785826261fb9169d5caa8ef87de41ce63317
jimmycababa <jimmycababa@gmail.com>
2021-04-23 20:24:31 -0500

    codewars 4/23

--- cw.js
+++ cw.js
@@ -646,3 +646,15 @@ function DNAtoRNA(dna) {
 function areYouPlayingBanjo(name) {
   return name.startsWith('r'.toUpperCase()) || name.startsWith('R'.toLowerCase()) ? name + " plays banjo" : name + " does not play banjo"
 }
+
+// Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+// Task
+// Your task is to return the correct string using the Template String Feature.
+// Input
+// Two Strings, no validation is needed.
+// Output
+// You must output a string containing the two strings with the word ```' are '```
+
+var TempleStrings = function (obj, feature){
+  return `${obj} are ${feature}`
+}
