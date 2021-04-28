ba61d9c556ebc8db5adb8cde3185cf01ce884f0f
jimmycababa <jimmycababa@gmail.com>
2021-04-28 09:19:31 -0500

    codewars 4/28

--- cw.js
+++ cw.js
@@ -797,3 +797,7 @@ let task = (w,n,c) => {
   let obj = {'Monday':'James','Tuesday':'John','Wednesday':'Robert','Thursday':'Michael','Friday':'William'};
   return `It is ${w} today, ${obj[w]}, you have to work, you must spray ${n} trees and you need ${n*c} dollars to buy liquid`;
 };
+// better solution
+function findNeedle(haystack) {
+  return "found the needle at position " + haystack.indexOf("needle");
+}
