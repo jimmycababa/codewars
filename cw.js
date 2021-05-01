b240706cc22c1e76e47dd87a5d9c04c2851ecbb4
jimmycababa <jimmycababa@gmail.com>
2021-04-30 22:33:16 -0500

    codewars 4/20

--- cw.js
+++ cw.js
@@ -837,3 +837,21 @@ function digitize(n){
 function digitize(n){
   return n.toString().reverse().map(Number)
 }
+
+// 4/30/2021
+// The first century spans from the year 1 up to and including the year 100, The second - from the year 101 up to and including the year 200, etc.
+// Given a year, return the century it is in.
+
+// Input , Output Examples :
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+// solution
+function century(year) {
+  let century = 1/100;
+  
+    return Math.ceil(century * year)
+
+}
