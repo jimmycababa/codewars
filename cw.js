95e9426f8f68a71ae7077774279812dfeeede94f
jimmycababa <jimmycababa@gmail.com>
2021-05-03 15:18:34 -0500

    codewars 5/3

--- cw.js
+++ cw.js
@@ -943,3 +943,15 @@ function average(scores) {
    return Math.round(answer / scores.length)
     
 }
+
+// Write a function that reverses the bits in an integer.
+
+// For example, the number 417 is 110100001 in binary. Reversing the binary is 100001011 which is 267.
+
+// You can assume that the number is not negative.
+
+// solution
+function reverseBits (n) {
+  let answer = n.toString(2);
+  return parseInt(answer.split('').reverse().join(''), 2)
+}
