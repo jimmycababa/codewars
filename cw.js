07959be5f8a1c6553601707897811e56ad68bf52
jimmycababa <jimmycababa@gmail.com>
2021-05-06 16:10:50 -0500

    codewars 5/6

--- cw.js
+++ cw.js
@@ -988,3 +988,16 @@ sortme = function(names){
 sortme = function(names){
   return names.sort()
 }
+
+// 5/6/2021
+// Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+// invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+// invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+// invert([]) == []
+// You can assume that all values are integers. Do not mutate the input array/list.
+
+// solution
+function invert(array) {
+ return array.map(x => -x)
+  }
