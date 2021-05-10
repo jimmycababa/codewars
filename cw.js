dfa2c9fe60d01cbc0bfdcded654812d5f2d04e7d
jimmycababa <jimmycababa@gmail.com>
2021-05-09 19:42:53 -0500

    codewars 5/9

--- cw.js
+++ cw.js
@@ -1032,3 +1032,11 @@ function getRealFloor(n) {
 function remove(s){
   return s.replace(/!$/, '')
 }
+
+// 5/9/2021
+// You are running the calculation 2 + 2 * 2 + 2 * 2 and expect to get the answer 32 but instead the function keeps returning 10. Fix the function to make it return 32 without changing the number or the operators.
+
+// solution
+function orderOperations () {
+  return (2 + 2) * (2 + 2) * 2
+}
