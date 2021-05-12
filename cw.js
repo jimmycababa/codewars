e51de3bf6de00173eabf43a29bca10aef27e1c7d
jimmycababa <jimmycababa@gmail.com>
2021-05-11 22:44:43 -0500

    codewars 5/11

--- cw.js
+++ cw.js
@@ -1065,3 +1065,16 @@ function addBinary(a,b) {
   }
   // better solution
   function validateCode (code) {return /^[123]/.test(code)}
+
+  // Write a simple regex to validate a username. Allowed characters are:
+
+// lowercase letters,
+// numbers,
+// underscore
+// Length should be between 4 and 16 characters (both included).
+
+// solution
+function validateUsr(username) {
+  const res = /^[0-9a-z_]{4,16}$/.test(username) 
+  return res
+}
