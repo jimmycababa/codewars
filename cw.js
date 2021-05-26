a7a52f2354fe59b5fcaef3b0446d47cc50951a55
jimmycababa <jimmycababa@gmail.com>
2021-05-26 15:21:27 -0500

    codewars 5/26

--- cw.js
+++ cw.js
@@ -1328,3 +1328,18 @@ function _if(bool, func1, func2) {
 function unusualFive() {
   return "answr".length
 }
+
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+function oddOrEven(array) {
+}
+
+// solution
+function oddOrEven(array) {
+  let answer = array.reduce((a,c) => a + c, 0);
+    return answer % 2 == 0 ? "even" : "odd"
+  }
