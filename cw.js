453ba1d28f3bcaacd6f976176eb232d203fb156b
jimmycababa <jimmycababa@gmail.com>
2021-05-25 21:40:39 -0500

    codewars 5/25

--- cw.js
+++ cw.js
@@ -1309,3 +1309,12 @@ function wordsToSentence(words) {
 function xor(a, b) {
   return Boolean(a) !== Boolean(b) ? true : false
 }
+
+// Create a function called _if which takes 3 arguments: a boolean value bool and 2 functions (which do not take any parameters): func1 and func2
+
+// When bool is truth-ish, func1 should be called, otherwise call the func2.
+
+// solution
+function _if(bool, func1, func2) {
+  return Boolean(bool) ? func1() : func2();
+}
