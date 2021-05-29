d21d01486442a9df9813b71eebe1fd16e05eecf3
jimmycababa <jimmycababa@gmail.com>
2021-05-28 21:30:15 -0500

    codewars 5/28

--- cw.js
+++ cw.js
@@ -1382,3 +1382,20 @@ function oddOrEven(array) {
     
     return newArray;
   }
+
+  // 5/28/2021
+  // find the position
+  // When provided with a letter, return its position in the alphabet.
+
+// Input :: "a"
+
+// Ouput :: "Position of alphabet: 1"
+
+function position(letter){
+}
+
+// solution
+function position(letter){
+  const alphabet = ["","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+return 'Position of alphabet:' + " " + alphabet.indexOf(letter)
+}
