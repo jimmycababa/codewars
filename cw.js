bc16e415e5d744b28cbc660b3cb58cc81f7e6ef0
jimmycababa <jimmycababa@gmail.com>
2021-06-04 13:49:56 -0500

    codewars 6/4

--- cw.js
+++ cw.js
@@ -1432,3 +1432,15 @@ The order of the sequence has to stay the same.
   // solution
   function distinct(a) {
     return [...new Set(a)];
+
+    // 6/4/2021
+    // return negative (refactor)
+    
+    // In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+    // The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+
+// solution
+function makeNegative(num) {
+  return Math.abs(num) * -1.
+}
