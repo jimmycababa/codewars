fd58004b3a7e496e7ce365c4d43d4b5491c36f97
jimmycababa <jimmycababa@gmail.com>
2021-06-09 09:28:19 -0500

    codewars 6/8

--- cw.js
+++ cw.js
@@ -1474,7 +1474,7 @@ function sumPPG(playerOne, playerTwo){
     // The conversion rate you should use is 6.75 CNY for every 1 USD. All numbers shold be rounded to the nearest hundredth (e.g. 21.00 NOT 21.0 or 21) (HINT: google toFixed)
 
     function usdcny(usd){
-      
+
     }
 
     // solution
@@ -1482,3 +1482,38 @@ function sumPPG(playerOne, playerTwo){
       let cny = usd * 6.75
       return cny.toFixed(2) + ' ' + 'Chinese Yuan'
     }
+
+    // 6/8/2021
+    // Return the day
+    // Complete the function which returns the weekday according to the input number:
+
+// 1 returns "Sunday"
+// 2 returns "Monday"
+// 3 returns "Tuesday"
+// 4 returns "Wednesday"
+// 5 returns "Thursday"
+// 6 returns "Friday"
+// 7 returns "Saturday"
+// Otherwise returns "Wrong, please enter a number between 1 and 7"
+
+
+    // solution
+    function whatday(num) { 
+      if(num == 1){
+        return "Sunday"
+      }else if(num == 2){
+        return "Monday"
+      }else if(num == 3){
+        return "Tuesday"
+      }else if(num == 4){
+        return "Wednesday"
+      }else if(num == 5){
+        return "Thursday"
+      }else if(num == 6){
+        return "Friday"
+      }else if(num == 7){
+        return "Saturday"
+      }else{
+        return "Wrong, please enter a number between 1 and 7"
+      }
+      
