2b4fa85090da1677c0210e6ae090b35ff9c43e39
jimmycababa <jimmycababa@gmail.com>
2021-06-13 18:44:11 -0500

    codewars 6/13

--- cw.js
+++ cw.js
@@ -1545,3 +1545,17 @@ var findAverage = function (nums) {
   let sum = nums.reduce((a,b) => a + b, 0)
   return sum / nums.length
 }
+
+// 6/13/2021
+// Say hello
+// Write a function to greet a person. Function will take name as input and greet the person by saying hello. Return null/nil/None if input is empty string or null/nil/None.
+
+function greet(name){
+  
+}
+
+
+// solution
+function greet(name) {
+  return name ? `hello ${name}!` : null
+  }
