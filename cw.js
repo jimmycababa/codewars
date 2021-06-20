bac1e6d81c114336395a43fc3cff097f337766ed
jimmycababa <jimmycababa@gmail.com>
2021-06-20 17:28:24 -0500

    codewars 6/20

--- cw.js
+++ cw.js
@@ -1635,3 +1635,16 @@ var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
 function grow(x){
   return x.reduce((a,b) => a*b)
   }
+
+  // 6/21/2021
+  // is n divisible by x and y
+// Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero digits.
+
+function isDivisible(n,x,y){
+  
+}
+
+  // solution
+  function isDivisible(n, x, y) {
+    return n % y== 0 && n % x == 0 ? true : false
+  }
