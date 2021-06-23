82dfcce2489b32f269adc14c3c45c1ff678ad71f
jimmycababa <jimmycababa@gmail.com>
2021-06-23 11:56:52 -0500

    codewars 6/23

--- cw.js
+++ cw.js
@@ -1684,3 +1684,41 @@ function volume(r,h) {
   const pi = 3.14159
   return Math.floor(pi * r * r * h / 3)
   }
+
+  // 6/23/2021
+  // Grasshopper - terminal game function
+  // You are creating a text-based terminal version of your favorite board game. In the board game, each turn has six steps that must happen in this order: roll the dice, move, combat, get coins, buy more health, and print status.
+
+// You are using a library (Game.Logic in C#) that already has the functions below. Create a function named doTurn/DoTurn/do_turn that calls the functions in the proper order as described in the paragraph above.
+
+// - combat
+// - buyHealth
+// - getCoins
+// - printStatus
+// - rollDice
+// - move
+
+
+// solution
+function doTurn () {
+  rollDice()
+ move()
+ combat()
+ getCoins()
+ buyHealth()
+ printStatus()
+}
+
+// FizzBuzz - without ifs
+// You need to implement a function that applies the standard fizz buzz test:
+
+// If a number is divisible by 3, return Fizz
+// If it is divisible by 5, return Buzz
+// If it is divisible by both 3 and 5, return FizzBuzz
+// If it is not divisible by 3 or 5, return the number itself
+// However, when making the function, you cannot use if (meaning else is also not an option).
+
+// solution
+function fizzBuzz(n) {
+  return n % 3 == 0 && n % 5 == 0 ? "FizzBuzz" : n % 3 == 0 ? "Fizz" : n % 5 == 0 ? "Buzz" : n
+};
