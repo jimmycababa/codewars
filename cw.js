3b7c9170ef57e2e3ded1df41641fd55eaafa649d
jimmycababa <jimmycababa@gmail.com>
2021-06-27 09:06:08 -0500

    codewars 6/27

--- cw.js
+++ cw.js
@@ -1735,3 +1735,21 @@ async function promiseHelloWorld() {
   // replace this nonsense with your Promise
   return 'Hello World!'
 }
+
+// 6/27/2021
+// Fix the Bugs (Syntax) - My First Kata
+// Hello, this is my first Kata so forgive me if it is of poor quality.
+
+// In this Kata you should fix/create a program that returns the following values:
+
+// false/False if either a or b (or both) are not numbers
+// a % b plus b % a if both arguments are numbers
+
+// solution
+function myFirstKata(a,b) {
+  if (typeof(a) !== "number" || typeof(b) !== "number") {
+    return false;
+  } else {
+    return (a % b)+ (b % a);
+  }
+}
