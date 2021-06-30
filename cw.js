b79f8f8474838279ac9cd5cbbfcc02517ae94808
jimmycababa <jimmycababa@gmail.com>
2021-06-30 09:32:19 -0500

    codewars 6/30

--- cw.js
+++ cw.js
@@ -1808,3 +1808,12 @@ function summation(num) {
   return num * (num + 1) / 2
 }
 
+// 6/30/2021
+// Function 1 = hello world
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// Style Points
+// Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What is a "hello world" solution you would want to show your friends?
+
+// solution
+const greet = () => "hello world!"
