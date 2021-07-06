a3debb28cbd7c13520023cb0ab5cfb68fde555ca
jimmycababa <jimmycababa@gmail.com>
2021-07-05 20:13:07 -0500

    codewars 7/5

--- cw.js
+++ cw.js
@@ -1858,3 +1858,18 @@ function pointsPer48(ppg, mpg) {
 function opposite(number) {
   return number ? -number : number
 }
+
+//Count the Monkeys
+// You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
+
+// As a good parent, you will sit and count with him. Given the number (n), populate an array with all numbers up to and including that number, but excluding zero.
+
+// For example:
+
+// monkeyCount(10) // --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+// monkeyCount(1) // --> [1] 
+
+// solution
+function monkeyCount(n) {
+  return  Array.from(new Array(n), (x,i) => i+1)
+  }
