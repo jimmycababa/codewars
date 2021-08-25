44614592555c05dd327d1e5d2b218f6583f8fee3
jimmycababa <jimmycababa@gmail.com>
2021-08-24 21:08:38 -0500

    invert values ruby codewars 8/24

---
+++ Ruby Codewars/Invert values.rb
@@ -0,0 +1,12 @@
+# Invert values - Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+# problem
+def invert(list)
+
+    end
+
+    # solution
+    def invert(list)
+        list.map do |x| x*-1
+      end
+        end
