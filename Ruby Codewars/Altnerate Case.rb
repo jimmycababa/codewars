2dc6cb8ad0f574d050e12429d2cef28ada5ddd64
jimmycababa <jimmycababa@gmail.com>
2021-08-27 09:39:03 -0500

    alternate case ruby codewars 8/27

---
+++ Ruby Codewars/Altnerate Case.rb
@@ -0,0 +1,10 @@
+# Alternate Case - Write function alternateCase which switch every letter in string from upper to lower and from lower to upper. E.g: Hello World -> hELLO wORLD
+
+# problem
+def alternate_case(s)
+  end
+
+#   solution
+def alternate_case(s)
+    s.swapcase
+  end
---
+++ Ruby Codewars/altERnaTIng cAsE <=> ALTerNAtiNG CaSe.rb
@@ -0,0 +1,13 @@
+# altERnaTIng cAsE <=> ALTerNAtiNG CaSe - Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase.
+
+# problem
+class String
+    def to_alternating_case
+    end
+
+    # solution
+    class String
+        def to_alternating_case
+         swapcase
+        end
+        end
