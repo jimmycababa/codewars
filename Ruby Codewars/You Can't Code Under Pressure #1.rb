e8f0021a44156767bad7efb2e8f9cb7411f889e6
jimmycababa <jimmycababa@gmail.com>
2021-09-02 14:21:46 -0500

    you cant code under pressure #1 ruby codewars 9/2

---
+++ Ruby Codewars/You Can't Code Under Pressure #1.rb
@@ -0,0 +1,11 @@
+# You Can't Code Under Pressure #1 - Code as fast as you can! You need to double the integer and return it.
+
+# problem
+def double_integer(i)
+    #Double the input!
+  end
+
+#   solution
+def double_integer(i)
+    i * 2
+  end
