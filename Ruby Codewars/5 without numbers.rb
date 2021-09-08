db4d2cbb36bd70fc095ad9c8aa603a5db8c342fb
jimmycababa <jimmycababa@gmail.com>
2021-09-07 21:00:33 -0500

    5 without numbers ruby codewars 9/7

---
+++ Ruby Codewars/5 without numbers.rb
@@ -0,0 +1,15 @@
+# 5 without numbers - Write a function that always returns 5
+
+# Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+# Good luck :)
+
+
+# problem
+def unusual_five
+  end 
+
+# solution
+def unusual_five
+    "fiver".length
+  end 
