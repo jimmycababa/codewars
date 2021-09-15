d60039755fd66d7f04a58c2241665b39b23629d5
jimmycababa <jimmycababa@gmail.com>
2021-09-14 23:13:22 -0500

    the least and greatest ruby codewars 9/15

---
+++ Ruby Codewars/Enumerable Magic #27 - The Least and the Greatest.rb
@@ -0,0 +1,10 @@
+# Enumerable Magic #27 - The Least and the Greatest - Create a method minmax that accepts a list and a block. The block itself should compare two elements, so the method can sort the elements and return the minimum and maximum as a 2-element array.
+
+# problem
+def minmax list, &block
+  end
+
+# solution
+def minmax list, &block
+    list.minmax(&block)
+  end
