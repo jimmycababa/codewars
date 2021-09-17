b051a350b954eb5c0046ad5613a961cfd807b3bc
jimmycababa <jimmycababa@gmail.com>
2021-09-17 15:34:17 -0500

    array to hash ruby codewars 9/17

---
+++ Ruby Codewars/ Enumerable Magic #31 - Array to Hash.rb
@@ -0,0 +1,16 @@
+# Enumerable Magic #31 - Array to Hash - Create a method to_h that accepts a list, and converts it to a hash of key-value pairs.
+
+# Here's a simple example:
+
+# animals = [["cat", "dog"], ["duck", "cow"]]
+# to_h(animals)
+#     #=> {"cat" => "dog", "duck" => "cow"}
+
+# problem
+def to_h list
+  end
+
+# solution
+def to_h list
+    list.to_h
+  end
