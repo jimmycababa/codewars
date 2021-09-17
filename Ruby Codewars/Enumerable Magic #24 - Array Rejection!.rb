46e2598fb18cf80f453c16515936a7891a725299
jimmycababa <jimmycababa@gmail.com>
2021-09-17 15:30:35 -0500

    array rejection ruby codewars 0/17

---
+++ Ruby Codewars/Enumerable Magic #24 - Array Rejection!.rb
@@ -0,0 +1,10 @@
+# Enumerable Magic #24 - Array Rejection! - Create a method reject that accepts a list and a block, and returns a list of elements for which the block returns false.
+
+# problem
+def reject list, &block
+  end
+
+# solution
+def reject list, &block
+    list.reject(&block)
+  end
