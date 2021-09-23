ed9752419dbbb180da68d155f0d9b5daa23fe1ba
jimmycababa <jimmycababa@gmail.com>
2021-09-22 20:39:50 -0500

    all items except the first ruby codewars 9/22

---
+++ Ruby Codewars/Enumerable Magic #18 - All Items Except the First...rb
@@ -0,0 +1,10 @@
+# Enumerable Magic #18 - All Items Except the First.. - Create a method drop that accepts a list, and a number n, and returns a copy of the list that skips the first n elements.
+
+# problem
+def drop list, n
+  end
+
+# solution
+def drop list, n
+    list.drop(n)
+  end
