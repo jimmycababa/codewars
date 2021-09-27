3ed5ed61e58821fd778414d61513ee1bf2c0f4b8
jimmycababa <jimmycababa@gmail.com>
2021-09-27 14:50:17 -0500

    printing arrays with comma delimiters ruby codewars 9/27

---
+++ Ruby Codewars/Printing Array elements with Comma delimiters.rb
@@ -0,0 +1,16 @@
+# Printing Array elements with Comma delimiters - Input: Array of elements
+
+# ["h","o","l","a"]
+
+# Output: String with comma delimited elements of the array in th same order.
+
+# "h,o,l,a"
+
+# problem
+def printArray(array)
+  end
+
+# solution
+def printArray(array)
+    array.join(',')
+  end
