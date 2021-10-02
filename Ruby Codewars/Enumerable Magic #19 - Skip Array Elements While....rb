e58021f74970dea5587731a03c5eb4a7a6f14fb8
jimmycababa <jimmycababa@gmail.com>
2021-10-01 22:54:25 -0500

    skip elements while ruby codewars 10/1

---
+++ Ruby Codewars/Enumerable Magic #19 - Skip Array Elements While....rb
@@ -0,0 +1,10 @@
+# Enumerable Magic #19 - Skip Array Elements While... - Create a method drop_while that accepts a list and a block, and returns a copy of the list that skips over elements from the left, for as long as the given block returns true.
+
+# problem
+def drop_while list, &block
+  end
+
+# solution
+def drop_while list, &block
+    list.drop_while(&block)
+  end
