f1b3986717f2cfc570855bf4493cd4334439334f
jimmycababa <jimmycababa@gmail.com>
2021-10-09 08:47:48 -0700

    find the max value ruby codewars 10/9

---
+++ Ruby Codewars/Enumerable Magic #10 - Find the Maximum Value.rb
@@ -0,0 +1,10 @@
+# Enumerable Magic #10 - Find the Maximum Value - Create a method max that accepts a list and a block, and returns the item that is the maximum using the block as the comparator.
+
+# problem
+def max list, &block
+    end
+
+# solution
+def max list, &block
+    list.max(&block)
+    end
