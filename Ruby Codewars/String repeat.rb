36c6bf2bb0b9773756d64a31ccf434f61a444c8d
jimmycababa <jimmycababa@gmail.com>
2021-10-21 15:11:46 -0500

    string repeat ruby codewars 10/21

---
+++ Ruby Codewars/String repeat.rb
@@ -0,0 +1,10 @@
+# String repeat - Write a function called repeatStr which repeats the given string string exactly n times.
+
+# problem
+def repeat_str (n, s)
+    end
+
+# solution
+def repeat_str (n, s)
+    s * n
+    end
